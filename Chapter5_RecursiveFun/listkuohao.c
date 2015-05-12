d7178b967efeb1078922d81ed4b81b5be62d5bbb
yzk2014new <bluenight04521@hotmail.com>
2015-05-12 23:44:25 +0800

    fix code format

--- Chapter5_RecursiveFun/listkuohao.c
+++ Chapter5_RecursiveFun/listkuohao.c
@@ -19,19 +19,19 @@ void listKuoHao(int n) {
 }
 
 void printKuoHao(char *str, int n) {
-	//printf("printKuoHao: %s, %d\n", str, n);
+    //printf("printKuoHao: %s, %d\n", str, n);
     if (str == 0 || n < 0 || n > STRING_LENGTH / 2) {
         printf("input is invalid.\n");
         return;
     }
     if (n == 1 && strlen(str) == 0) {
-		printf("()\n");
+        printf("()\n");
 	} else if (n == 0) {
-		printf("%s\n", str);
+        printf("%s\n", str);
     } else {
         char *newStr1 = (char *)malloc(STRING_LENGTH);
         char *newStr2 = (char *)malloc(STRING_LENGTH);
-		char *newStr3 = (char *)malloc(STRING_LENGTH);
+        char *newStr3 = (char *)malloc(STRING_LENGTH);
         if (newStr1 == 0 || newStr2 == NULL || newStr3 == NULL) {
             printf("alloc memory failed.\n");
             return;
@@ -40,23 +40,23 @@ void printKuoHao(char *str, int n) {
         strcat(newStr1, "()");
         strcpy(newStr2, "()");
         strcat(newStr2, str);
-		strcpy(newStr3, "(");
+        strcpy(newStr3, "(");
         strcat(newStr3, str);
         strcat(newStr3, ")");
-		printKuoHao(newStr1, n - 1);
+        printKuoHao(newStr1, n - 1);
         if (strcmp(newStr1, newStr2) == 0) {
-			if (strcmp(newStr1, newStr3) != 0) {
-				printKuoHao(newStr3, n - 1);
-			}
+            if (strcmp(newStr1, newStr3) != 0) {
+                printKuoHao(newStr3, n - 1);
+            }
         } else {
-			printKuoHao(newStr2, n - 1);
-			if (strcmp(newStr1, newStr3) != 0 && strcmp(newStr2, newStr3) != 0) {
-				printKuoHao(newStr3, n - 1);
-			}
+            printKuoHao(newStr2, n - 1);
+            if (strcmp(newStr1, newStr3) != 0 && strcmp(newStr2, newStr3) != 0) {
+                printKuoHao(newStr3, n - 1);
+            }
         }
-		free(newStr1);
+        free(newStr1);
         free(newStr2);
-		free(newStr3);
+        free(newStr3);
     }
 }
 
